14803277b1e63cdab54ef63d3579d38f9efcf17b
DianaB177 <anaidb77@gmail.com>
2023-01-16 16:16:16 +0100

    prueba api disease.sh

--- Dad Jokes/script.js
+++ Dad Jokes/script.js
@@ -11,9 +11,9 @@ async function generateJoke() {
     },
   }
 
-  const res = await fetch('https://icanhazdadjoke.com', config);
+  const res = await fetch('https://disease.sh/v3/covid-19/all', config);
   const data = await res.json();
-  jokeEl.innerHTML = data.joke;
+  jokeEl.innerHTML = data.deaths;
 }
 generateJoke();
 
